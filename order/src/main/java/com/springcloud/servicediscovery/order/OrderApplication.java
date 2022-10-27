8d9db97b6d05bc6e16b1a11f1fc544ceff4606ea
Numero Uno <numerounodeveloper@gmail.com>
2022-10-27 11:05:09 -0400

    Using Eureka Service Discovery

--- order/src/main/java/com/springcloud/servicediscovery/order/OrderApplication.java
+++ order/src/main/java/com/springcloud/servicediscovery/order/OrderApplication.java
@@ -2,7 +2,10 @@ package com.springcloud.servicediscovery.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- order/src/main/java/com/springcloud/servicediscovery/order/controller/OrderController.java
+++ order/src/main/java/com/springcloud/servicediscovery/order/controller/OrderController.java
@@ -2,18 +2,24 @@ package com.springcloud.servicediscovery.order.controller;
 
 import com.springcloud.servicediscovery.order.dao.OrderRepo;
 import com.springcloud.servicediscovery.order.model.Order;
+import com.springcloud.servicediscovery.paymnt.model.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+
 
 @RestController
 public class OrderController {
     @Autowired
     private OrderRepo repo;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/orders")
     private List<Order> getAllStudents(){
@@ -22,6 +28,14 @@ public class OrderController {
 
     @PostMapping("/orders")
     private Order addOrder(@RequestBody Order newOrder){
+        Order ordResp = repo.save(newOrder);
+
+        Payment pmt = new Payment();
+        pmt.setOrderId(ordResp.getOrderId());
+        pmt.setPaymentAmt(ordResp.getPrice() * ordResp.getQuantity());
+
+        Payment pmResp = restTemplate.postForObject("http://payment/payments", pmt, Payment.class);
+        //Payment pmResp = restTemplate.postForObject("http://localhost:9002/payments", pmt, Payment.class);
         return repo.save(newOrder);
     }
 }
